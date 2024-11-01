cf176e37d5685fb4a0dace339456451e9a84d519
Bruno <rigalrigal2014@outlook.fr>
2024-11-01 14:49:41 +0100

    Mise à jour de style.css, index.html, et app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Bienvenue sur votre espace musical privé !");
+const players = Plyr.setup('.player', { 
+    controls: ['play', 'progress', 'volume', 'download'],
+    i18n: {
+        restart: 'Redémarrer',
+        rewind: 'Reculer {seektime}s',
+        play: 'Lire',
+        pause: 'Pause',
+        fastForward: 'Avancer {seektime}s',
+        mute: 'Muet',
+        unmute: 'Son activé',
+        volume: 'Volume',
+        download: 'Télécharger',
+        settings: 'Paramètres',
+        fullscreen: 'Plein écran'
+    }
 });
+
